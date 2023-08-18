a58eb5f8dd6efc9bfa5437a6c8e4aedb5b730f30
noahblob <noahhagardent@gmail.com>
2023-08-18 14:23:55 +1200

    feat: Created connection to the MongoDB database

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// configures so we can have environment variables in .env file
+require('dotenv').config();
+
+// create express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+// cors middleware and express that allows us to parse json
+app.use(cors());
+app.use(express.json());
+
+// start connection to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
